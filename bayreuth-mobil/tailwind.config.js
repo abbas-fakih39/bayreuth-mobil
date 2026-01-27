c5699bbcaa788bc2442b861888302cb6ab27fc58
Abbas Fakih <abbassfakih54321@outlook.com>
2026-01-27 19:36:43 +0100

    page accueil

---
+++ bayreuth-mobil/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          green: '#9EC446',
+          blue: '#1A6B8F',
+          dark: '#114B63',
+          light: '#F0F9FB',
+        }
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'Roboto'], 
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
